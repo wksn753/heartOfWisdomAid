18a7636fab06e6073ff922646bb5326d602ab668
wksn753 <wksn75321@gmail.com>
2024-07-29 23:01:56 +0300

    service

--- HOWSA/src/pages/Programs/components/Service.tsx
+++ HOWSA/src/pages/Programs/components/Service.tsx
@@ -7,6 +7,7 @@ const Service = ({ serviceImage, serviceTitle, serviceText,classname }: { servic
             <div className="service-text flow">
                 <h3 className="fs-600 fw-bold">{serviceTitle}</h3>
                 <p>{serviceText}</p>
+                
             </div>
             </div>
     );
